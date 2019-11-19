2b614fc32f490c801f035c3a2540f37a603c7b20
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-11-18 20:27:35 -0600

    se reciben los datos del wifi

--- SMARTHOUSEARDUINO.ino
+++ SMARTHOUSEARDUINO.ino
@@ -1,9 +1,25 @@
+class sensor{
+  public:
+  int pin;
+   String tipo;
+   float valor;
+  
+  };
+
+sensor* temperatura=new sensor[20];
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(115200);
+Serial.setTimeout(200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("S");
+  while(!Serial.available());
+  String confi=Serial.readString();
+
+  
+  
 
 }
