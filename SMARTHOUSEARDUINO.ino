1f747f6598ecb9942dffa93d84e6ab69d028bab3
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-11-24 01:00:31 -0600

    se arreglo loo de la temp

--- SMARTHOUSEARDUINO.ino
+++ SMARTHOUSEARDUINO.ino
@@ -1,37 +1,44 @@
+#include <Servo.h>
+
 #include <DHT.h>
 //claseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/////////////////////////////////////////////////////////////////////
+
 class Habitacion {
 
 
   public:
     int pinT, pinM, pinL, pinV, pinP, id;
     String nombre;
-    float valorT, valorM, valorL, valorV, valorP;
+    float valorT, valorM, valorL, valorV, valorP, temperatura;
+    Servo servo;
 
 
 
 
     Habitacion() {}
-    Habitacion(String _nombre, int _pinT, int _pinM, int _pinV, int _pinP, int _pinL, int _id ) {
+    Habitacion(String _nombre, int _pinT, int _pinM, int _pinV, int _pinP, int _pinL, int _id ,float _temperatura) {
       id = _id;
       pinT = _pinT;
       pinM = _pinM;
       pinV = _pinV;
       pinL = _pinL;
+      temperatura=_temperatura;
 
       pinP = _pinP;
       nombre = _nombre;
       valorT = 0.0;
-      valorM = 0.0;
+      valorM = 1.0;
       valorL = 0.0;
       valorV = 0.0;
       valorP = 0.0;
 
       pinMode(pinT, INPUT);
-      pinMode(pinM, INPUT);
+      pinMode(pinM, OUTPUT);
       pinMode(pinL, INPUT);
       pinMode(pinV, INPUT);
       pinMode(pinP, INPUT);
+      servo.attach(pinM);
+      servo.write(1.0);
 
 
 
@@ -43,9 +50,11 @@ class Habitacion {
       dht.begin();
       delay(100);
       float s = dht.readTemperature();
-      if(isnan(s)){
-        s=valorT;
-        }else{valorT=s;}
+      if (isnan(s)) {
+        s = valorT;
+      } else {
+        valorT = s;
+      }
       return s  ;
     }
 
@@ -57,6 +66,8 @@ class Habitacion {
 int contadorHabitaciones;
 Habitacion habitaciones[30];
 int flatThread;
+int hora = 0, minuto = 0, segundo = 0;
+
 
 
 
@@ -83,6 +94,7 @@ void loop() {
 
 
   long timee = micros();
+
   if (flatThread == 0) {
     //hilo uno para enviar datos de senores
     flatThread = 1;
@@ -98,20 +110,86 @@ void loop() {
 
 
     }
+
     Serial.print("A");
     delay(10);
     Serial.print(data);
   }
+
+
+
+
+
   timee = micros();
   if (flatThread == 1) {
+    //hilo para checar temperatura
     flatThread = 2;
 
 
     while (micros() < timee + 200000) {
 
+      for (int x = 0; x < contadorHabitaciones; x++) {
+
+        if (habitaciones[x].valorT < habitaciones[x].temperatura) {
+          //la habitacion es menor a la que se requiere
+          //se debe cerrar ventanas
+
+
+          if (1.1 < (habitaciones[x].temperatura - habitaciones[x].valorT) < 3) {
+            habitaciones[x].valorM -= (habitaciones[x].valorT * .25);
+
+
+          } else if (3.1 < (habitaciones[x].temperatura - habitaciones[x].valorT) < 4) {
+            habitaciones[x].valorM -= (habitaciones[x].valorT * .50);
+
+          } else if (4.1 < (habitaciones[x].temperatura - habitaciones[x].valorT) < 5) {
+            habitaciones[x].valorM -= (habitaciones[x].valorT * .75);
+
+          } else if (5 < (habitaciones[x].temperatura - habitaciones[x].valorT)) {
+            habitaciones[x].valorM = 180;
+          }
+          if ( habitaciones[x].valorM <= 0) {
+            habitaciones[x].valorM = 1;
+          }
+
+        } else {
+
+          //la temperatra es mayor a la que se requiere
+          //se debe abrir ventanas
+          if (habitaciones[x].valorM == 0) {
+            habitaciones[x].valorM++;
+          }
+
+
+          if (1.1 < (habitaciones[x].valorT - habitaciones[x].temperatura) < 3) {
+
+            habitaciones[x].valorM += (habitaciones[x].valorT * .25);
+
+          } else if (3.1 < (habitaciones[x].valorT - habitaciones[x].temperatura) < 4) {
+            habitaciones[x].valorM += (habitaciones[x].valorT * .5);
+          } else if (4.1 < (habitaciones[x].valorT - habitaciones[x].temperatura) < 5) {
+            habitaciones[x].valorM += (habitaciones[x].valorT * .75);
+          } else if (5 < (habitaciones[x].valorT - habitaciones[x].temperatura)) {
+            habitaciones[x].valorM = 1;
+          }
+
+
+          if ( habitaciones[x].valorM >= 180) {
+            habitaciones[x].valorM = 180;
+          }
+        }
+
+        habitaciones[x].servo.write(habitaciones[x].valorM);
+
+      }
+
+
     }
 
   }
+
+
+
 
 
   timee = micros();
@@ -166,7 +244,7 @@ void getConf() {
     char *token2;
     token2 = strtok(str2, s2);
 
-    String z[7];
+    String z[8];
     int p = 0;
     while ( token2 != NULL ) {
       z[p] = String(token2);
@@ -178,7 +256,7 @@ void getConf() {
 
     }
 
-    habitaciones[y] = Habitacion(String(z[0]), String(z[1]).toInt(), String(z[2]).toInt(), String(z[3]).toInt(), String(z[4]).toInt(), String(z[5]).toInt(), String(z[6]).toInt());
+    habitaciones[y] = Habitacion(String(z[0]), String(z[1]).toInt(), String(z[2]).toInt(), String(z[3]).toInt(), String(z[4]).toInt(), String(z[5]).toInt(), String(z[6]).toInt(), String(z[7]).toInt());
     contadorHabitaciones++;
 
 
